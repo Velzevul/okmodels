239e138cb8efca0ffeda36d299b4c4ce67c0b5cd
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2013-01-10 12:27:28 +0200

    Initialized repository with gems and rspec

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.9'
+gem 'sqlite3'
+gem 'paperclip'
+
+group :test, :development do
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'factory_girl_rails'
+  gem 'capybara'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Okmodelagency::Application.config.secret_token = '6ef304a709ca7ea3dcd3460750b393e85cfacb054e43af48afd7ad752ad35be4c8764bfb7c305bf57f2689bcd02329174c22655e24bb8eadacc675a7b6f8822a'
