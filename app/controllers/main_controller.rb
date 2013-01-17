b8bc56ba289a6f6aab14f733f58b5e8b81a13261
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2013-01-17 13:50:55 +0200

    changed latest action in main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,8 +6,6 @@ class MainController < ApplicationController
   end
 
   def latest
-    @men = Man.latest(10)
-    @women = Woman.latest(10)
-    @children = Child.latest(10)
+    @models = Model.latest(25)
   end
 end
--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -3,12 +3,10 @@ require 'spec_helper'
 describe MainController do
 
   describe "GET 'latest'" do
-    %w(man woman child).each do |category|
-      it "retrieves latest 10 #{category.pluralize}" do
-        entries = FactoryGirl.create_list(category, 11)
-        get :latest
-        assigns(category.pluralize).should eq(entries[1..11].reverse)
-      end
+    it "retrieves latest 25 models " do
+      entries = FactoryGirl.create_list(:model, 26)
+      get :latest
+      assigns(:models).should eq(entries[1..26].reverse)
     end
   end
 
