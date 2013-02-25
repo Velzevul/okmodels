78a935ea4dcebde62736b2f6466fd667b118ae34
Volodymyr Dziubak <Volodymyr.Dzyubak@gmail.com>
2013-02-25 22:40:25 +0100

    added photo scaling to max 800px, decreased max file size to 200kb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,11 +3,12 @@ class Photo < ActiveRecord::Base
   attr_accessible :image, :snapshot
 
   has_attached_file :image, 
-    :styles => { :thumb => "250x250^" }, 
+    :styles => { :original => "533x800>",
+    	           :thumb => "250x250^" }, 
     :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
     :url => "/system/:attachment/:id/:style/:filename"
 
   validates_attachment  :image, :presence => true,
                         :content_type => { :content_type => ["image/jpg","image/jpeg","image/gif"] },
-  									    :size => { :in => 0..2.megabytes }
+  									    :size => { :in => 0..200.kilobytes }
 end
